beb745529602a175b121cf4acd13bbbf12fc5715
megagao <megagao@126.com>
2017-04-07 22:14:32 +0800

    快速排序

---
+++ src/leetcode/first/QuickSort.java
@@ -0,0 +1,40 @@
+package leetcode.first;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QuickSort {
+
+	public static void sort(int[] A, int low, int high) {
+		if(low >= high){
+			return;
+		}
+		int i = low;
+		int j = high;
+		int index = A[i];
+		while(i<j){
+			while(i<j&&A[j]>=index)
+				j--;
+			if(i<j)
+				A[i++] = A[j];
+			while(i<j && A[i]<index)
+				i++;
+			if(i<j)
+				A[j--] = A[i];
+		}
+		A[i] = index;
+		sort(A,low,i-1);
+		sort(A,i+1,high);
+	}
+	
+    public static void main(String[] args) throws IOException{
+    	/*Scanner cin=new Scanner(System.in);
+    	int L = cin.nextInt();
+    	int R = cin.nextInt();*/
+    	int[] A = {2676,4662,8383,357,6595};
+    	sort(A, 0, 4);
+    	System.out.println(A);
+    }
+}
+
